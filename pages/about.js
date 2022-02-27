51e66bd568a45ef848200dd40746d1a07d1881e3
Eddy Alvarado <eddyalvarado1124@gmail.com>
2022-02-27 11:05:14 -0600

    initial

---
+++ pages/about.js
@@ -0,0 +1,19 @@
+import Head from "next/head";
+
+const About = () => {
+    return (
+        <>
+            <Head>
+                <title>Ninja List | About</title>
+            <meta name="keywords" content="ninjas2" />
+            </Head>
+            <div>
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe eius corrupti dolor itaque odit aperiam incidunt provident dicta distinctio dolorem minus nemo alias eligendi dolorum nisi, sapiente vel sequi eaque!</p>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe eius corrupti dolor itaque odit aperiam incidunt provident dicta distinctio dolorem minus nemo alias eligendi dolorum nisi, sapiente vel sequi eaque!</p>
+            </div>
+        </>
+    );
+}
+ 
+export default About;
